306efb2a39ca68604a569c54b36f7000acc03b6a
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-05-17 20:01:31 +0200

    Modify user model

--- djangobnb_backend/useraccount/models.py
+++ djangobnb_backend/useraccount/models.py
@@ -1,3 +1,15 @@
+import uuid
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, UserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def _create_user(self, name, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have not specified a valid email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
