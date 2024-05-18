aa691ef6586fd10d440deff43226f260b4b1cdff
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-05-18 12:44:11 +0200

    Add media url to settings

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -50,6 +50,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
 ]
+
+REST_AUTH = {"USE_JWT": True, "JWT_AUTH_HTTPONLY": False}
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -59,6 +63,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "allauth",
+    "allauth.account",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+    "corsheaders",
     "useraccount",
     "property",
 ]
@@ -66,6 +78,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -144,6 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
