5dabc3ac07e1e2506a9cd5e823510d405fb921cd
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-05-18 12:57:28 +0200

    Create properties model

--- djangobnb_backend/property/models.py
+++ djangobnb_backend/property/models.py
@@ -1,3 +1,24 @@
+import uuid
+from django.conf import settings
+from useraccount.models import User
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=225)
+    description = models.TextField()
+    price_per_night = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    guests = models.IntegerField()
+    country = models.CharField(max_length=225)
+    country_code = models.CharField(max_length=10)
+    category_code = models.CharField(max_length=10)
+    image = models.ImageField(max_length=255)
+    address = models.TextField()
+    landlord = models.ForeignKey(
+        User, related_name="properties", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
