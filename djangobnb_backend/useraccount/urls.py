ba6825fceeadc9e16096e92c82b99a12a8a2c99c
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-05-19 15:50:13 +0200

    Add auth path to main url

--- djangobnb_backend/useraccount/urls.py
+++ djangobnb_backend/useraccount/urls.py
@@ -7,4 +7,6 @@ from rest_framework_simplejwt.views import TokenVerifyView
 
 urlPatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
