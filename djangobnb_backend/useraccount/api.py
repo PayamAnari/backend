eac8eac9d0fc2ef47e74cc2072ba4d46fff80312
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-05-29 20:40:20 +0200

    Create landlord detail def

--- djangobnb_backend/useraccount/api.py
+++ djangobnb_backend/useraccount/api.py
@@ -1,2 +1,18 @@
 from .models import User
 from .serializers import UserDetailSerializer
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permissions_classes,
+)
+from django.http import JsonResponse
+
+
+@api_view(["GET"])
+@authentication_classes([])
+@permissions_classes([])
+def landlord_detail(request, pk):
+    user = User.objects.get(pk=pk)
+    serializer = UserDetailSerializer(user, many=False)
+
+    return JsonResponse(serializer.data, safe=False)
