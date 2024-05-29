209cfebcc3816535bbfe46325df8def1262623d3
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-05-29 21:03:11 +0200

    modify properties_list api

--- djangobnb_backend/useraccount/api.py
+++ djangobnb_backend/useraccount/api.py
@@ -3,14 +3,14 @@ from .serializers import UserDetailSerializer
 from rest_framework.decorators import (
     api_view,
     authentication_classes,
-    permissions_classes,
+    permission_classes,
 )
 from django.http import JsonResponse
 
 
 @api_view(["GET"])
 @authentication_classes([])
-@permissions_classes([])
+@permission_classes([])
 def landlord_detail(request, pk):
     user = User.objects.get(pk=pk)
     serializer = UserDetailSerializer(user, many=False)
--- djangobnb_backend/useraccount/urls.py
+++ djangobnb_backend/useraccount/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("<uuid=pk>/", api.landlord_detail, name="api_landlord_detail"),
+    path("<uuid:pk>/", api.landlord_detail, name="api_landlord_detail"),
 ]
