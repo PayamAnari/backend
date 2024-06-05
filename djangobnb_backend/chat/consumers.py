bf7385c20bca4c08f29bd85914ef9f4fcab8b1c5
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-06-05 13:13:23 +0200

    Create connect def

--- djangobnb_backend/chat/consumers.py
+++ djangobnb_backend/chat/consumers.py
@@ -4,3 +4,13 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .models import ConversationMessage
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
