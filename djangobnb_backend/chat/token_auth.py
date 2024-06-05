58068b253e73fe861a469230af662d34a8f54a4a
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-06-05 13:05:05 +0200

    Create TokenAuthMiddleware

--- djangobnb_backend/chat/token_auth.py
+++ djangobnb_backend/chat/token_auth.py
@@ -17,3 +17,14 @@ def get_user(token_key):
         return User.objects.get(pk=user_id)
     except Exception as e:
         return AnonymousUser()
+
+
+class TokenAuthMiddleware(BaseMiddleware):
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        query = dict((x.split("=") for x in scope["query_string"].decode().split("&")))
+        token_key = query.get("token")
+        scope["user"] = await get_user(token_key)
+        return await super().__call__(scope, receive, send)
