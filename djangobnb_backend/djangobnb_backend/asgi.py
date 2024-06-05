7ac56cf35ad759f33e1f430ba7da655d8ca17e88
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-06-05 12:44:23 +0200

    modify application

--- djangobnb_backend/djangobnb_backend/asgi.py
+++ djangobnb_backend/djangobnb_backend/asgi.py
@@ -9,3 +9,14 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangobnb_backend.settings")
 
 application = get_asgi_application()
+
+from chat import routing
+from chat.token_auth import TokenAuthMiddleware
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": TokenAuthMiddleware(URLRouter(routing.websocket_urlpatterns)),
+    }
+)
