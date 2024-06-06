d7ab5b15396bedae350a7fc959c8a2678fb58e6d
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-06-06 13:50:27 +0200

    Create send message def

--- djangobnb_backend/chat/consumers.py
+++ djangobnb_backend/chat/consumers.py
@@ -1,6 +1,6 @@
 import json
 
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .models import ConversationMessage
@@ -34,3 +34,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
         await self.save_message(conversation_id, body, sent_to_id)
+
+    # Sending messages
+    async def chat_message(self, event):
+        body = event["body"]
+        name = event["name"]
+
+        await self.send(text_data=json.dumps({"body": body, "name": name}))
+
+    @sync_to_async
+    def save_message(self, conversation_id, body, sent_to_id):
+        user = self.scope["user"]
+
+        ConversationMessage.objects.create(
+            conversation_id=conversation_id,
+            body=body,
+            sent_to_id=sent_to_id,
+            created_by=user,
+        )
