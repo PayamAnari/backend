c13e6d6bd6c587499d1cdfd70fdce8bf36436da9
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-07-02 18:11:29 +0200

    Add create review url

--- djangobnb_backend/review/urls.py
+++ djangobnb_backend/review/urls.py
@@ -4,4 +4,5 @@ from . import api
 
 urlpatterns = [
     path("<uuid:property_id>/reviews/", api.get_reviews, name="api_get_reviews"),
+    path("reviews/", api.create_review, name="api_create_review"),
 ]
