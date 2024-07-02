893cdd7f5d3af1ad211e93fc0d0c97b4b24cceb4
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-07-02 18:10:39 +0200

    Create review api

--- djangobnb_backend/review/api.py
+++ djangobnb_backend/review/api.py
@@ -15,3 +15,14 @@ def get_reviews(request, property_id):
     reviews = Review.objects.filter(property_id=property_id)
     serializer = ReviewSerializer(reviews, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(["POST"])
+@permission_classes([])
+@authentication_classes([])
+def create_review(request):
+    serializer = ReviewSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, safe=False)
+    return JsonResponse(serializer.errors, status=400)
