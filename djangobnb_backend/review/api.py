d795ff674cb23cabc3858cbb37e72ab07509583b
Payam Anari <125816107+PayamAnari@users.noreply.github.com>
2024-07-02 12:42:40 +0200

    Modify api

--- djangobnb_backend/review/api.py
+++ djangobnb_backend/review/api.py
@@ -1,10 +1,16 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
 from .models import Review
 from .serializers import ReviewSerializer
 
 
 @api_view(["GET"])
+@permission_classes([])
+@authentication_classes([])
 def get_reviews(request):
     serializer = ReviewSerializer(Review.objects.all(), many=True)
 
